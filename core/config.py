2d4d7607272fad3342242b09ac2c605c03647d90
Chawanakorn Akkayachinda <yecgaa2344@gmail.com>
2025-04-20 17:52:49 +0700

    fix: update config

--- core/config.py
+++ core/config.py
@@ -5,3 +5,4 @@ load_dotenv(override=True)  # Loads variables from .env into os.environ
 
 MONGODB_URI = os.getenv("MONGODB_URI")
 FIREBASE_CERT_PATH = os.getenv("FIREBASE_CERT_PATH")
+FIREBASE_CREDENTIAL = os.getenv("FIREBASE_CREDENTIAL")
--- core/firebase.py
+++ core/firebase.py
@@ -1,8 +1,26 @@
 import firebase_admin
 from firebase_admin import credentials, auth
-from core.config import FIREBASE_CERT_PATH
+from core.config import FIREBASE_CERT_PATH, FIREBASE_CREDENTIAL
+import os
+import json
 
-cred = credentials.Certificate(FIREBASE_CERT_PATH)
+firebase_key_path = FIREBASE_CERT_PATH
+firebase_key_env = FIREBASE_CREDENTIAL
+
+if firebase_key_env:
+    # Step 1: Parse the string into a dictionary
+    cred_dict = json.loads(firebase_key_env)
+
+    # Step 2: Fix the private_key newlines
+    cred_dict["private_key"] = cred_dict["private_key"].replace("\\n", "\n")
+
+    # Step 3: Pass it to Firebase
+    cred = credentials.Certificate(cred_dict)
+elif firebase_key_path and os.path.exists(firebase_key_path):
+    cred = credentials.Certificate(firebase_key_path)
+else:
+    raise Exception("No Firebase credentials found.")
+#cred = credentials.Certificate(FIREBASE_CERT_PATH)
 firebase_admin.initialize_app(cred)
 
 # def verify_token(id_token: str):
